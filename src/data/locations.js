aa482d937dd8d3de3dc7a038784b58ef5c355125
truefisherking <truefisherking@gmail.com>
2025-02-07 14:10:52 -0500

    Updated the store numbers

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,10 +1,10 @@
 export const locations = [
-    { id: 1, value: "Branford, CT"},
-    { id: 2, value: "Derby, CT"},
-    { id: 3, value: "Fairfield, CT"},
-    { id: 4, value: "Hamden, CT"},
+    { id: 514, value: "Branford, CT"},
+    { id: 510, value: "Derby, CT"},
+    { id: 507, value: "Fairfield, CT"},
+    { id: 4515, value: "Hamden, CT"},
     { id: 513, value: "Milford, CT"},
-    { id: 6, value: "North Haven, CT"},
+    { id: 528, value: "North Haven, CT"},
     { id: 532, value: "Orange, CT"},
 ]
 // Get store codes for each location
